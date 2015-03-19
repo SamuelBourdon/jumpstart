7985d8686ff07c0cee98a0e4b0b68e4421d999ab
SamuelBourdon <samuel.bourdon@gmail.com>
2015-03-19 12:22:44 +0100

    Add tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+  validates :blurb, :length => { :in => 10..255 },
+                    :format => { :with => /^\A[a-zA-Z\d ]+\z/,
+                    :message => "Only letters allowed",
+                    :multiline => true}
+  validates :title, :body, :presence => true
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 class PostsControllerTest < ActionController::TestCase
   setup do
     @post = posts(:one)
+    @update = {
+      title: 'Here is the title',
+      blurb: 'Here is the blurb',
+      body: 'Here is the body'
+    }
   end
 
   test "should get index" do
@@ -18,7 +23,7 @@ class PostsControllerTest < ActionController::TestCase
 
   test "should create post" do
     assert_difference('Post.count') do
-      post :create, post: { blurb: @post.blurb, body: @post.body, title: @post.title }
+      post :create, post: @update
     end
 
     assert_redirected_to post_path(assigns(:post))
@@ -35,7 +40,7 @@ class PostsControllerTest < ActionController::TestCase
   end
 
   test "should update post" do
-    patch :update, id: @post, post: { blurb: @post.blurb, body: @post.body, title: @post.title }
+    patch :update, id: @post, post: @update
     assert_redirected_to post_path(assigns(:post))
   end
 
